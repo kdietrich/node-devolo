f3f361657dc503778676aa22bf4013512be24b8d
Nico Hartung <nicohartung1@googlemail.com>
2018-10-05 18:09:44 +0200

    Added more variants of Danfoss thermostat valve

--- lib/Devolo.ts
+++ lib/Devolo.ts
@@ -221,7 +221,8 @@ export class Devolo {
                                 device = new RelaySwitchXDevice();
                 }
                 // Danfoss thermostat valve / radiator thermostat | https://products.z-wavealliance.org/regions/1/categories/10/products?company=3
-                else if((item.properties.deviceModelUID.indexOf('devolo.model.Unknown:Device') > -1) && (
+                else if(((item.properties.deviceModelUID.indexOf('devolo.model.Unknown:Device') > -1) ||
+                         (item.properties.deviceModelUID.indexOf('unk.model.Unknown:Device') > -1)) && (
                             (item.properties.prodID == '0x0004' && item.properties.prodTypeID == '0x0005') ||
                             (item.properties.prodID == '0x0003' && item.properties.prodTypeID == '0x0005') ||
                             (item.properties.prodID == '0xA010' && item.properties.prodTypeID == '0x0248') ||
